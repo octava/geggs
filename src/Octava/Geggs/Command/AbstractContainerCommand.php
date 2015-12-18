ec480efeb1e3b9074e9e9b34460ea4181f78f50a
Vladimir Pak <TheRatW@gmail.com>
2015-12-18 09:39:58 +0200

    refactor

--- src/Octava/Geggs/Command/AbstractContainerCommand.php
+++ src/Octava/Geggs/Command/AbstractContainerCommand.php
@@ -1,13 +1,8 @@
 <?php
 namespace Octava\Geggs\Command;
 
-use Octava\Geggs\DependencyInjection\OctavaGeggsExtension;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * Class AbstractContainerCommand
@@ -36,24 +31,5 @@ abstract class AbstractContainerCommand extends Command
     public function getContainer()
     {
         return $this->container;
-    }
-
-    protected function initialize(InputInterface $input, OutputInterface $output)
-    {
-        $filename = $input->getOption('config-file');
-        if (!file_exists($filename)) {
-            throw new \RuntimeException(sprintf('Config file "%s" not found', $filename));
-        }
-
-        //http://symfony.com/doc/2.8/components/dependency_injection/compilation.html
-        $container = $this->getContainer();
-
-        $extension = new OctavaGeggsExtension();
-        $container->registerExtension($extension);
-
-        $loader = new YamlFileLoader($container, new FileLocator(dirname($filename)));
-        $loader->load(basename($filename));
-
-        $container->compile();
     }
 }
--- src/Octava/Geggs/Command/StatusCommand.php
+++ src/Octava/Geggs/Command/StatusCommand.php
@@ -5,6 +5,9 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessBuilder;
 
 /**
  * Class StatusCommand
@@ -16,13 +19,62 @@ class StatusCommand extends AbstractContainerCommand
     {
         $this
             ->setName('status')
-            ->addArgument('pathspec', InputArgument::IS_ARRAY, 'pathspec');
+            ->addArgument('pathspec', InputArgument::IS_ARRAY, 'pathspec')
+            ->setDescription('Git status');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        $io->title('git status');
+        $io->title($this->getDescription());
 
+        $config = $this->getContainer()->get('octava_geggs.config');
+        $paths = $input->getArgument('pathspec');
+
+        $cmd = $this->buildCommand($config->getBin(), $config->getMainDir(), $paths);
+        $io->section('Main directory');
+        $io->note($config->getMainDir());
+        $this->runCommand($cmd, $io);
+    }
+
+    /**
+     * @param $bin
+     * @param $dir
+     * @param $paths
+     * @return string
+     */
+    protected function buildCommand($bin, $dir, array $paths = [])
+    {
+        $builder = new ProcessBuilder();
+        $builder->setPrefix($bin);
+        $builder
+            ->add('status');
+        foreach ($paths as $path) {
+            $builder->add($path);
+        }
+
+        $cmd = $builder->getProcess()
+            ->getCommandLine();
+        $cmd = 'cd '.$dir.' && '.$cmd;
+
+        return $cmd;
+    }
+
+    /**
+     * @param $cmd
+     * @param $io
+     */
+    protected function runCommand($cmd, $io)
+    {
+        $process = new Process($cmd);
+        $process->setTimeout(3600);
+        $process->setIdleTimeout(60);
+        try {
+            $process->mustRun();
+
+            $io->text($process->getOutput());
+        } catch (ProcessFailedException $e) {
+            $io->error($e->getMessage());
+        }
     }
 }
