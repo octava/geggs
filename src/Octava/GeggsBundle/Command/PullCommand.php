6035d91f4f74e3ce17dd2ed090d89f327cff6134
Dmitry Kuznetsov <d.kuznetsov@roboforex.com>
2016-01-28 15:51:12 +0000

    cosmetic changes

--- src/Octava/GeggsBundle/Command/PullCommand.php
+++ src/Octava/GeggsBundle/Command/PullCommand.php
@@ -3,9 +3,13 @@ namespace Octava\GeggsBundle\Command;
 
 use Octava\GeggsBundle\Helper\RepositoryFactory;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Monolog\Processor\MemoryPeakUsageProcessor;
+use Symfony\Bridge\Monolog\Handler\ConsoleHandler;
+use Symfony\Bridge\Monolog\Logger;
 
 /**
  * Class PullCommand
@@ -17,24 +21,23 @@ class PullCommand extends ContainerAwareCommand
     {
         $this
             ->setName('pull')
-            ->setDescription('Fetch from and integrate with another repository or a local branch');
+            ->setDescription('Fetch from and integrate with another repository or a local branch')
+            ->addArgument('remote-branch', InputArgument::OPTIONAL, 'Name of a branch in the remote repository');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $logger = new Logger($this->getName());
+        $logger->pushHandler(new ConsoleHandler($output));
+        $logger->pushProcessor(new MemoryPeakUsageProcessor());
+        $logger->debug('Start', ['command_name' => $this->getName()]);
+
         $io = new SymfonyStyle($input, $output);
 
         $config = $this->getContainer()->get('octava_geggs.config');
-        $factory = new RepositoryFactory($this->getContainer()->get('octava_geggs.config'));
+        $factory = new RepositoryFactory($this->getContainer()->get('octava_geggs.config'), $logger);
         $list = $factory->buildRepositoryModelList();
 
-        foreach ($list->getAll() as $item) {
-            $io->section($item->getPackageName());
-            $io->note($item->getPath());
-
-//            $process = $status->run($item->getAbsolutePath());
-
-            $io->writeln($process->getOutput());
-        }
+        $logger->debug('Finish', ['command_name' => $this->getName()]);
     }
 }
